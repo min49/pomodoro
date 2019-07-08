340470adfb580321f99ce57cf13b9b5bcc625f16
Min Thu Aung <reoknite@gmail.com>
2019-07-07 22:03:33 -0400

    Updates

---
+++ precache-manifest.2ed055c6a2ed4a07f5689c9185b87c5d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b470d9437d1fb66c8e6664e2422fb4",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "2bd944b18e3f044bc7d4",
+    "url": "/pomodoro/static/js/2.3b5de94c.chunk.js"
+  },
+  {
+    "revision": "c464411c1f3b8bc0d8f2",
+    "url": "/pomodoro/static/js/main.3ebc01a8.chunk.js"
+  },
+  {
+    "revision": "8d8194bde761299ff809",
+    "url": "/pomodoro/static/js/runtime~main.5f1f8454.js"
+  }
+]);
