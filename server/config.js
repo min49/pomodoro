af346f8c09d4d64066778296b482250ebda13889
Min Thu Aung <reoknite@gmail.com>
2019-07-11 18:01:54 -0400

    backend init

---
+++ server/config.js
@@ -0,0 +1,3 @@
+require('dotenv').config(); // Read env variables from .env file
+
+exports.MONGODB_URI = process.env.MONGODB_URI;
---
+++ server/server.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const port = process.env.PORT || 3000;
+const app = require('./app');
+
+app.listen(port, function() {
+  console.log(`Node.js listening on ${port}`);
+});
